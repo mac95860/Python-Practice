fbf3714b7618c639e9a34196b98599c2331bc5ca
Michael Cervantes <mac95860@hotmail.com>
2021-04-26 12:16:17 -0500

    more datetime

--- date-time-module.py
+++ date-time-module.py
@@ -1,4 +1,5 @@
 import datetime
+import pytz
 
 today = datetime.date.today()
 print(today)
@@ -10,4 +11,27 @@ days_since_birth = (today - birthday).days
 print(days_since_birth)
 
 tdelta = datetime.timedelta(days=10)
-print(today + tdelta)
+print(today - tdelta)
+
+print(today.month)
+# Monday is beginning of the week and has value of 0
+print(today.weekday())
+
+# hour, minute, second, milisecond
+
+print(datetime.time(7, 2, 20, 15))
+# datetime.date (Y, M, D)
+# datetime.time (H, M, S, MS)
+# datetime.datetime (Y, M, D, h , m , s, ms)
+
+# add 10 hours to current day
+hour_delta = datetime.timedelta(hours=10)
+print(datetime.datetime.now() + hour_delta)
+
+datetime_today = datetime.datetime.now(tz = pytz.UTC)
+datetime_pacific = datetime_today.astimezone(pytz.timezone('US/Pacific'))
+print(datetime_pacific)
+
+for tz in pytz.all_timezones:
+    print(tz)
+
